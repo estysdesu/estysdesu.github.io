660311c243037207627016f3f80a0fa5df391848
Tyler Estes <estysdesu@gmail.com>
2019-04-15 19:00:24 +0900

    add path check for instance config for production

--- mySite/__init__.py
+++ mySite/__init__.py
@@ -1,11 +1,13 @@
 from flask import Flask
+import os
 
 # init the flask app
 mySiteApp = Flask(__name__, instance_relative_config=True)
 
 # config
 mySiteApp.config.from_object("config")
-mySiteApp.config.from_pyfile("config.py")
+if os.path.exists("instance"):
+    mySiteApp.config.from_pyfile("config.py")
 
 # import modules
 from . import models, views
